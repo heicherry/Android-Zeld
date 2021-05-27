118d74082ae14da8fd9dd654ada5f2535b2251ae
Jimmy Ai <2625171028@qq.com>
2021-05-27 09:08:32 +0800

    feat:游戏场景旁白开发

--- commonbusiness/playground/src/main/java/com/ai/zeld/playground/BaseBusinessSection.kt
+++ commonbusiness/playground/src/main/java/com/ai/zeld/playground/BaseBusinessSection.kt
@@ -9,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.ImageView
+import com.ai.zeld.common.basesection.ext.speakWaitForClick
 import com.ai.zeld.common.basesection.section.BaseSection
 import com.ai.zeld.common.service.menu.IMenu
 import com.ai.zeld.common.service.world.IWorld
@@ -17,10 +18,16 @@ import com.ai.zeld.util.app.App
 import com.ai.zeld.util.claymore.load
 import com.ai.zeld.util.gone
 import com.ai.zeld.util.idToBitmap
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.xmlpull.v1.XmlPullParser
 
 abstract class BaseBusinessSection : BaseSection(), IGameResult {
     private val succeedBitmap = R.drawable.playground_succeed.idToBitmap()
     private val failedBitmap = R.drawable.playground_failed.idToBitmap()
+
+    private var needShowPrologue = true
 
     override fun onFailed() {
     }
@@ -79,8 +86,40 @@ abstract class BaseBusinessSection : BaseSection(), IGameResult {
         return R.drawable.playground_cover_sample
     }
 
+    protected open fun getPrologueXmlId() = R.xml.prologue
+
     override fun onSectionEnter() {
         super.onSectionEnter()
         IWorld::class.java.load().lockSection(getSectionId(), false)
+        val prologueId = getPrologueXmlId()
+        if (prologueId != -1) {
+            startTalkPrologue(parsePrologueXml(prologueId))
+        }
+    }
+
+    private fun startTalkPrologue(prologue: List<String>) {
+        GlobalScope.launch(Dispatchers.Main) {
+            prologue.forEach {
+                speakStage.speakWaitForClick("", it, 2000)
+            }
+        }
+    }
+
+    private fun parsePrologueXml(xmlId: Int): List<String> {
+        val parser = localContext.resources.getXml(xmlId)
+        val speech = mutableListOf<String>()
+        while (parser.eventType != XmlPullParser.END_DOCUMENT) {
+            when (parser.eventType) {
+                XmlPullParser.START_TAG -> {
+                    when (parser.name) {
+                        "node" -> {
+                            speech.add(parser.getAttributeValue(null, "text"))
+                        }
+                    }
+                }
+            }
+            parser.next()
+        }
+        return speech
     }
 }
